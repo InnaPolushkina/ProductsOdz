f0cdfe33182ee0d77409c652b46eede96494cba0
InnaPolushkina <innachka.polushkina@gmail.com>
2019-05-15 11:43:39 +0300

    finished commit

--- src/com/polushkina/inna/controller/Main.java
+++ src/com/polushkina/inna/controller/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
    @Override
     public void start(Stage primaryStage) throws Exception{
-       Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
+       Parent root = FXMLLoader.load(getClass().getResource("/com/polushkina/inna/view/sample.fxml"));
        primaryStage.setTitle("Products");
        primaryStage.setScene(new Scene(root, 900, 820));
        primaryStage.show();
